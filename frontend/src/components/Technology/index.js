b8ea837927155b7922cd1f0d336571d66270e19b
vallas01 <vallasac@gmail.com>
2022-06-13 12:24:11 -0400

    tech used list complete

--- frontend/src/components/Technology/index.js
+++ frontend/src/components/Technology/index.js
@@ -7,30 +7,43 @@ const Technology = () => {
             <div className='headingTwo'>Technologies used for this project</div>
 
 
-            <div className='tech-div'>
-                <i class="fa-brands fa-js fa-2xl"></i>
-                <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' >Javascript</a>
-
-                <i class="fa-brands fa-html5 fa-2xl"></i>
-                <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/HTML' >HTML Documentation</a>
-
-                <i class="fa-brands fa-css3-alt fa-2xl"></i>
-                <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/css' >CSS Documentation</a>
-
-                <i class="fa-brands fa-font-awesome fa-2xl"></i>
-                <a className="developer-link" href='https://fontawesome.com/' >Font Awesome</a>
-
-                <i class="fa-brands fa-node fa-2xl"></i>
-                <a className="developer-link" href='https://expressjs.com/en/api.html' >Express</a>
-
-                <i class="fa-brands fa-react fa-2xl"></i>
-                <a className="developer-link" href='https://reactjs.org/docs/getting-started.html' >React Documentation</a>
-
-                <i class="fa-brands fa-reacteurope fa-2xl"></i>
-                <a className="developer-link" href='https://redux.js.org/' >Redux Documentation</a>
-
-                <i class="fa-solid fa-database fa-2xl"></i>
-                <a className="developer-link" href='https://www.postgresql.org/' >PostgreSQL</a>
+            <div className='tech-container'>
+                <div className='tech'>
+                    <i class="fa-brands fa-js fa-2xl"></i>
+                    <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/JavaScript' >Javascript</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-css3-alt fa-2xl"></i>
+                    <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/css' >CSS Documentation</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-html5 fa-2xl"></i>
+                    <a className="developer-link" href='https://developer.mozilla.org/en-US/docs/Web/HTML' >HTML Documentation</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-reacteurope fa-2xl"></i>
+                    <a className="developer-link" href='https://redux.js.org/' >Redux Documentation</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-react fa-2xl"></i>
+                    <a className="developer-link" href='https://reactjs.org/docs/getting-started.html' >React Documentation</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-font-awesome fa-2xl"></i>
+                    <a className="developer-link" href='https://fontawesome.com/' >Font Awesome</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-solid fa-database fa-2xl"></i>
+                    <a className="developer-link" href='https://www.postgresql.org/' >PostgreSQL</a>
+                </div>
+                <div className='tech'>
+                    <i class="fa-brands fa-node fa-2xl"></i>
+                    <a className="developer-link" href='https://expressjs.com/en/api.html' >Express</a>
+                </div>
+                <div className='tech tech-heroku'>
+                    <img className='heroku' src='./heroku.png' alt="logo"></img>
+                    <a className="heroku-link" href='https://www.heroku.com/' >Heroku</a>
+                </div>
             </div>
 
 
