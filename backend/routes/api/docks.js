44f2b8cb67764b4709ac9400f43542b99f960660
vallas01 <vallasac@gmail.com>
2022-06-22 14:52:12 -0400

    form error handling done

--- backend/routes/api/docks.js
+++ backend/routes/api/docks.js
@@ -14,13 +14,48 @@ const validateDockForm = [
       .notEmpty()
       .isLength({min: 4})
       .withMessage('Please provide a longer name.'),
-    check('state')
+    check('address')
       .exists({ checkFalsy: true })
       .notEmpty()
       .isLength({min: 4})
-      .withMessage('Please provide a full state name.'),
-    handleValidationErrors,
-  ];
+      .withMessage('Please provide a longer address.'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({min: 4})
+      .withMessage('Please provide a longer city name.'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({min: 2})
+      .withMessage('Enter state name or 2 letter abbreviation.'),
+    check('cost')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isFloat({min: 0, max: 10})
+      .withMessage('Cost must be between 0 and $10.00.'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({min: 4})
+      .withMessage('Add at least a one word description.'),
+    check('latitude')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isFloat({min: -90, max: 90})
+      .withMessage('Latitude must be between -90 and 90.'),
+    check('longitude')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isFloat({min: -90, max: 90})
+      .withMessage('Longitude must be between -180 and 180.'),
+    check('imagePath')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isLength({min: 5})
+      .withMessage('Add an image path.'),
+      handleValidationErrors,
+    ];
 
   //get all docks
 router.get('/', asyncHandler(async function(req, res) {
