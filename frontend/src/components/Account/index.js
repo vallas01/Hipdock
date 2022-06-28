63e23ccbd815af50e4cb5da7d8cb6d750e47d063
vallas01 <vallasac@gmail.com>
2022-06-28 19:37:19 -0400

    date function complete

--- frontend/src/components/Account/index.js
+++ frontend/src/components/Account/index.js
@@ -33,10 +33,26 @@ const Account = () => {
     let foundBook = false;
 
     const formatDate = (date) =>{
-        let dd = date.split("T").slice(0,1);
-        // let y = date.slice(0,4);
-        // let m = date.slice(6,8);
-        return dd;
+        const months = {
+            '01': 'Jan. ',
+            '02': 'Feb. ',
+            '03': 'Mar. ',
+            '04': 'Apr. ',
+            '05': 'May ',
+            '06': 'Jun. ',
+            '07': 'Jul. ',
+            '08': 'Aug. ',
+            '09': 'Sep. ',
+            '10': 'Oct. ',
+            '11': 'Nov. ',
+            '12': 'Dec. ',
+          }
+
+        let y = date.slice(0,4);
+        let mRaw = date.slice(5,7);
+        let m = months[mRaw]
+        let d = date.slice(8,10);
+        return m + d + ', ' + y;
     }
     return (
         <div className='account-outer-container'>
